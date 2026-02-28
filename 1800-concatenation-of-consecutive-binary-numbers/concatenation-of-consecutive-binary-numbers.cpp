b4580daa7288c1d6bef1464a89c2e15bbad7582c
Abhishek01yadav <ay0180310@gmail.com>
2026-02-28 19:17:32 +0530

    Time: 45 ms (56.38%) | Memory: 8 MB (89.26%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int M = 1e9 + 7;
     int concatenatedBinary(int n) {
+        int digits=0;
 
         long result = 0;
-        for (int i = 1; i <= n; i++) {
-            int digit = log2(i) + 1; // kisi bhi number me kitne binary digits
+        for (int num = 1; num <= n; num++) {
+            if((num & num-1)==0) digits++; // kisi bhi number me kitne binary digits
                                      // honge ye ye btata hai.
-            result = ((result << digit) % M + i) %
-                     M; //(yha pr result<<digit) purane number kitne digit ka
+            result = ((result << digits) % M + num) %M;
+                      //(yha pr result<<digit) purane number kitne digit ka
                         //hoga ye btayega phir usme agar add kr denge naya
                         //number to phir hume new number mil jayega.
         }
