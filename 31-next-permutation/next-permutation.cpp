f6ab08e334c8fb56f85f71d128dba9dc828882c6
Abhishek01yadav <ay0180310@gmail.com>
2026-03-02 16:17:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (67.86%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,7 +2,29 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums ){
        
-        next_permutation(nums.begin(),nums.end());
-        
+       // next_permutation(nums.begin(),nums.end());
+       //first i have to find breaking point longest matching prefix
+       int n=nums.size();
+       int idx=-1;
+       for(int i=n-2;i>=0;i--){
+        if(nums[i]<nums[i+1]){
+            idx=i;
+            break;
+        }
+       }
+       if(idx==-1) 
+       {reverse(nums.begin(),nums.end());
+       return;
+       }
+       // dusra kaam us idx wale element se just bada element find krna hai.
+       for(int i=n-1;i>idx;i--){
+        if(nums[i]>nums[idx]){
+            swap(nums[i],nums[idx]);
+            break;
+        }
+       }
+       // teesra kaam ab sort krna hai idx element k udhar wala part
+       reverse(nums.begin()+idx+1,nums.end());
+        return;
     }
 };
