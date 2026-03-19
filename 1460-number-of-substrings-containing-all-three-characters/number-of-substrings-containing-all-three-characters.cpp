998ad20efd682d0c873a1f48c4d761939961beae
Abhishek01yadav <ay0180310@gmail.com>
2026-03-20 00:13:27 +0530

    Time: 11 ms (42.89%) | Memory: 11.1 MB (56.14%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.size();
-        vector<int> freq(3, 0);
-        int left = 0, ans = 0;
-
-        for(int right = 0; right < n; right++) {
-            freq[s[right] - 'a']++;
-
-            while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
-                ans += (n - right); // important trick
-                freq[s[left] - 'a']--;
-                left++;
-            }
-        }
-        return ans;
-     
         
+       // brute force
+        // int n=s.size();
+        // int cnt=0;
+        // for(int i=0;i<n;i++){
+        //     vector<int>hash(3,0);
+        //     for(int j=i;j<n;j++){
+        //         hash[s[j]-'a']=1;
+        //         if(hash[0]+hash[1]+hash[2]==3) 
+        // {
+        //     cnt=cnt+(n-j);
+        //     break;
+        // }
+
+                
+        //     }
+            
+        // }
+        
+        // return cnt;
+
+       // 2nd approach: sliding window
+       int n=s.size();
+       vector<int>lastseen(3,-1);
+       int cnt=0;
+       for(int i=0;i<n;i++){
+        lastseen[s[i]-'a']=i;
+        if(lastseen[0]!=-1 && lastseen[1]!=-1  && lastseen[2]!=-1){
+            int x= min(lastseen[0] ,lastseen[1]);
+            int y=min(x,lastseen[2]);
+            cnt=cnt+1+y;
+
+        }
+       }
+       return cnt;
+
+
     }
 };
