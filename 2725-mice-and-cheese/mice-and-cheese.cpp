8e83f78b4e634e7b609dfdb49c50323414ca55ec
Abhishek01yadav <ay0180310@gmail.com>
2026-03-20 09:29:22 +0530

    Time: 51 ms (25.65%) | Memory: 112.1 MB (37.31%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.cpp
+++ 2725-mice-and-cheese/mice-and-cheese.cpp
@@ -10,13 +10,23 @@ public:
             diff[i]= reward1[i]-reward2[i];
         }
 
-sort(diff.rbegin(),diff.rend());
-// priority_queue<int,vector<int>,greater<int>>pq;
-// for(int i=0;i<diff.s)
-for(int i=0;i<k;i++){
-    sum1+=diff[i];
-
+//sort(diff.rbegin(),diff.rend());
+priority_queue<int,vector<int>,greater<int>>pq;
+for(int i=0;i<diff.size();i++){
+    pq.push(diff[i]);
+    if(pq.size()>k){
+        pq.pop();
+    }
+    
 }
+while(!pq.empty()){
+    sum1+=pq.top();
+    pq.pop();
+}
+// for(int i=0;i<k;i++){
+//     sum1+=diff[i];
+
+// }
 return sum1;
 
 
