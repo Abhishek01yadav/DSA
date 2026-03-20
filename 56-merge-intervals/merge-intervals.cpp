0a4bfbcbbb3d394394dc9d28688915103299b995
Abhishek01yadav <ay0180310@gmail.com>
2026-03-20 12:26:23 +0530

    Time: 8 ms (25.33%) | Memory: 23.7 MB (80.97%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end());
+
+          int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        for(int i = 1; i < intervals.size(); i++) {
+            
+            if(intervals[i][0] <= end) {
+                // overlap → extend interval
+                end = max(end, intervals[i][1]);
+            } else {
+                // no overlap → push previous
+                ans.push_back({start, end});
+                
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        
+
+        ans.push_back({start, end});
+        
+        return ans;
+
+
+
+        
+    }
+};
