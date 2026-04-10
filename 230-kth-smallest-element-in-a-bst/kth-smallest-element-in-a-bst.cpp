ea457992f13fb461140f18d79a4ec14dc477d4c4
Abhishek01yadav <ay0180310@gmail.com>
2026-04-10 09:32:21 +0530

    Time: 3 ms (8.70%) | Memory: 24.6 MB (27.65%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,43 +1,44 @@
 
 class Solution {
 public:
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     TreeNode* curr = root;
+    //     while (curr != NULL) {
+    //         if (curr->left != NULL) { /
+    //             TreeNode* pred = curr->left;
+    //             while (pred->right && pred->right != curr) {
+    //                 pred = pred->right;
+    //             }
+    //             if (pred->right == NULL) { 
+    //                 pred->right = curr;
+    //                 curr = curr->left;
+    //             } else { 
+    //                 pred->right = NULL;
+    //                 ans.push_back(curr->val);
+    //                 curr = curr->right;
+    //             }
+    //         } else { // curr->left==NULL
+    //             ans.push_back(curr->val);
+    //             curr = curr->right;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    vector<int>ans;
+    void inorder(TreeNode* root){
+        if(root==NULL) return;
+        inorder(root->left);
+        ans.push_back(root->val);
+        inorder(root->right);
 
-vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-TreeNode* curr=root;
-        while(curr!=NULL){
-if(curr->left!=NULL){// find the pred
-TreeNode*  pred=curr->left;
-while(pred->right && pred->right !=curr){
-    pred=pred->right;
-}
-if(pred->right==NULL){//link
-pred->right=curr;
-curr=curr->left;
-}
-else{//pred->right=curr:unlink
-    pred->right=NULL;
-    ans.push_back(curr->val);
-    curr=curr->right;
 
-}
-}
-else{// curr->left==NULL
-ans.push_back(curr->val);
-curr=curr->right;
-}
-        }
-        return ans;
     }
 
     int kthSmallest(TreeNode* root, int k) {
-
-        vector<int>result;
-        result= inorderTraversal(root);
-        return result[k-1];
+        inorder( root);
 
         
-
-        
+        return ans[k - 1];
     }
 };
