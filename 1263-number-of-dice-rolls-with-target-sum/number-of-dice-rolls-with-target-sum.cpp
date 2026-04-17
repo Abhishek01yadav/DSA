c5edea7a537824a7ec58a0de894c92a2cdc627ce
Abhishek01yadav <ay0180310@gmail.com>
2026-04-17 18:41:14 +0530

    Time: 174 ms (5.33%) | Memory: 393.2 MB (5.08%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -2,30 +2,40 @@ class Solution {
 public:
 #define mod 1000000007
 #define ll long long int 
-vector<vector<ll>> dp = vector<vector<ll>>(1005, vector<ll>(1005, -1));
-ll solve(int target,int k,int n){
-    if(target==0) return 0;
-    if(n==1) 
-    {
-        return (target>=1 && target<=k)? 1:0;
-       
-    }
-    if(dp[target][n]!=-1) return dp[target][n];
-    
-    
-ll sum=0;
-   
+vector<vector<ll>> dp = vector<vector<ll>>(1005, vector<ll>(1005, -1));// note 2d dp kyuki sttate of dp n and target hai.
+// ll solve(int target,int k,int n){
+//     if(target==0) return 0;
+//     if(n==1)   return (target>=1 && target<=k)? 1:0; 
+//     if(dp[target][n]!=-1) return dp[target][n];
+//     ll sum=0;
+
+//     for(int i=1;i<=k;i++)
+//     {
+//         if(target -i <0) break;
+//      sum=(sum %mod +   solve(target-i,k,n-1) % mod )%mod;
+//     }
+
+//     return  dp[target][n]=sum %mod;
+// }
+ll f(int n,int k,int t){
+    if(n==0 and t==0) return 1;
+    if(n==0) return 0;
+    ll sum=0;
+    if (dp[t][n]!=-1) return dp[t][n];
     for(int i=1;i<=k;i++){
-        if(target -i <0) break;
-     sum=(sum %mod +   solve(target-i,k,n-1) % mod )%mod;
+        if(t-i < 0) break;
+        sum= (sum %mod + f(n-1,k,t-i)%mod ) %mod;
+
     }
- 
-   
-   
-    return  dp[target][n]=sum %mod;
+    return  dp[t][n]=sum %mod; 
+
+
+
 }
+
+
     int numRollsToTarget(int n, int k, int target) {
-        return solve(target,k,n);
+        return f(n,k,target);
         
     }
 };
