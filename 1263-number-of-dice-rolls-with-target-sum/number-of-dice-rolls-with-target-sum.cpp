caf75af5bec6ce4cfc643dac66709b48b892dd23
Abhishek01yadav <ay0180310@gmail.com>
2026-04-17 18:59:28 +0530

    Time: 184 ms (5.33%) | Memory: 393.5 MB (5.08%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 #define mod 1000000007
 #define ll long long int 
-vector<vector<ll>> dp = vector<vector<ll>>(1005, vector<ll>(1005, -1));// note 2d dp kyuki sttate of dp n and target hai.
+//vector<vector<ll>> dp = vector<vector<ll>>(1005, vector<ll>(1005, -1));// note 2d dp kyuki sttate of dp n and target hai.
 // ll solve(int target,int k,int n){
 //     if(target==0) return 0;
 //     if(n==1)   return (target>=1 && target<=k)? 1:0; 
@@ -17,17 +17,35 @@ vector<vector<ll>> dp = vector<vector<ll>>(1005, vector<ll>(1005, -1));// note 2
 
 //     return  dp[target][n]=sum %mod;
 // }
-ll f(int n,int k,int t){
-    if(n==0 and t==0) return 1;
-    if(n==0) return 0;
-    ll sum=0;
-    if (dp[t][n]!=-1) return dp[t][n];
-    for(int i=1;i<=k;i++){
-        if(t-i < 0) break;
-        sum= (sum %mod + f(n-1,k,t-i)%mod ) %mod;
+// ll f(int n,int k,int t){
+//     if(n==0 and t==0) return 1;
+//     if(n==0) return 0;
+//     ll sum=0;
+//     if (dp[t][n]!=-1) return dp[t][n];
+//     for(int i=1;i<=k;i++){
+//         if(t-i < 0) break;
+//         sum= (sum %mod + f(n-1,k,t-i)%mod ) %mod;
 
+//     }
+//     return  dp[t][n]=sum %mod; 
+
+
+
+// }
+
+ll fbu(int n,int k,int t){
+    vector<vector<ll>>dp= vector<vector<ll>>(1005,vector<ll>(1005,0));
+    dp[0][0]=1;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=t;j++){
+            for(int r=1;r<=k;r++){
+                if(j-r <0) break;
+                dp[i][j]+=(dp[i-1][j-r])%mod;
+            }
+        }
     }
-    return  dp[t][n]=sum %mod; 
+    return dp[n][t] % mod;
 
 
 
@@ -35,7 +53,7 @@ ll f(int n,int k,int t){
 
 
     int numRollsToTarget(int n, int k, int target) {
-        return f(n,k,target);
+        return fbu(n,k,target);
         
     }
 };
