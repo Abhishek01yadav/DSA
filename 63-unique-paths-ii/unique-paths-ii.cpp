f6423b999af08487a05c355b1d22456f2d7e8ea6
Abhishek01yadav <ay0180310@gmail.com>
2026-04-20 01:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (6.01%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
+vector<vector<int>>grid;
+int m,n;
 int dp[105][105];
-int f(vector<vector<int>>& obstacleGrid,int i,int j,int m,int n){
-    if (i >= m || j >= n) return 0;
+int f(int i,int j){
+    if (i >= m || j >= n) return 0;// ye dekh rha hai khi chalte chalte grid k bahar to nhi nikal gya 
 
-    // obstacle
-    if (obstacleGrid[i][j] == 1) return 0;
+    if (grid[i][j] == 1) return 0;
 int ans=0;
-    // destination
-    if (i == m-1 && j == n-1) return 1;
+    
+    if (i == m-1 && j == n-1) return 1;// ye mtlb valid hai ek rasta mil gya 
     if(dp[i][j]!=-1) return dp[i][j];
 
-    // move right + down
-     ans=f(obstacleGrid, i+1, j, m, n) +
-           f(obstacleGrid, i, j+1, m, n);
+ 
+     ans=f( i+1, j) +  f( i, j+1);// robot ko chala rhe hai
+           
 
            return dp[i][j]=ans;
     
 
 }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int  m=obstacleGrid.size();
-        int n =obstacleGrid[0].size();
+        grid=obstacleGrid;
+          m=grid.size();
+         n =grid[0].size();
         memset(dp,-1,sizeof dp);
-        return f(obstacleGrid,0,0,m,n);
+        return f(0,0);
         
     }
 };
