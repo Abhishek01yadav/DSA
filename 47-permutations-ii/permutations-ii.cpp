f4cfd4300102dd1ce954e91144dea02123482f87
Abhishek01yadav <ay0180310@gmail.com>
2026-06-09 12:38:17 +0530

    Time: 8 ms (24.03%) | Memory: 15 MB (21.66%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-         vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        do{
-            ans.push_back(nums);
-        }
-        while(next_permutation(nums.begin(),nums.end()));
-        return ans;
+void solve(int idx,vector<int>& nums, vector<vector<int>>&ans){
+    int n=nums.size();
+    if(idx==n){
+        ans.push_back(nums);
+        return ;
 
+    }
+    unordered_set<int>st;
 
+    for(int i=idx;i<n;i++){
+        if(st.find(nums[i])==st.end()){
+            st.insert(nums[i]);
         
+        swap(nums[i],nums[idx]);
+        solve(idx+1,nums,ans);
+        swap(nums[i],nums[idx]);
+        }
+    }
+}
+  
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+        vector<vector<int>>ans;
+        solve(0,nums,ans);
+        return ans;
     }
 };
