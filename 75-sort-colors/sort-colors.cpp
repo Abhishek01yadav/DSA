f5b07a5f354443de63e32c612807459c87f99dac
Abhishek01yadav <ay0180310@gmail.com>
2026-06-30 12:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.80%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -10,13 +10,13 @@ public:
         
         }
 
-        for(int i=0;i<nums.size();i++){
+      
             for(int j=0;j<cntz;j++) nums[j]=0;
              for(int k=cntz;k<(cnto+cntz);k++) nums[k]=1;
               for(int t=cnto+cntz;t<nums.size();t++) nums[t]=2;
 
 
-        }
+     
         
     }
 };
