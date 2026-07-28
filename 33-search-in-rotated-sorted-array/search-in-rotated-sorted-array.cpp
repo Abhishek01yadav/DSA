60146d0aad9384925e4b48805049906ae8903966
Abhishek01yadav <ay0180310@gmail.com>
2026-07-28 17:05:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (68.82%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
-    int findk(vector<int>& nums) {
-        int n = nums.size();
-        int k = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                k = i + 1;
-                break;
-            }
-        }
-        return k;
-    }
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
+        int n=nums.size();
+        int low=0,high=n-1;
 
-        int k = findk(nums);
+        while(low <= high){
+            int mid=low+(high-low)/2;
 
-        if (k > n)
-        {
-            k = k % n;
-        }
-
-        reverse(nums.begin(), nums.begin() + k);
-
-        reverse(nums.begin() + k, nums.end());
-
-        reverse(nums.begin(), nums.end());
-
-        int low = 0, high = n - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                return (k + mid)%n;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            if(nums[mid]==target){
+                return mid;
             }
-        }
+           
 
-        return -1;
+            if( nums[low] <=nums[mid]){
+                //left part is sorted
+
+                if(target>=nums[low] && target <nums[mid]){
+                    high=mid-1;
+                }
+
+                else{
+                    low=mid+1;
+                }
+
+            }
+       
+
+
+        else
+        {
+            if(target>nums[mid] && target<=nums[high]){
+            low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+
+        }
+        }
+    
+return -1;
+        
     }
 };
