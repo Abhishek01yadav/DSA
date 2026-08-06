d70754d5f435fa6c278e00c647173abbb47ee35f
Abhishek01yadav <ay0180310@gmail.com>
2026-08-07 00:13:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (97.55%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums ){
-       
-       // next_permutation(nums.begin(),nums.end());
-       //first i have to find breaking point longest matching prefix
-       int n=nums.size();
-       int idx=-1;
-       for(int i=n-2;i>=0;i--){
-        if(nums[i]<nums[i+1]){
-            idx=i;
-            break;
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int index=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]< nums[i+1])
+            {
+                index=i;
+                break;
+            }
+
+            
         }
-       }
-       if(idx==-1) 
-       {reverse(nums.begin(),nums.end());
+        
+        if(index==-1){
+            reverse(nums.begin(),nums.end());
+          return;
+        }
+
+
+        for(int i=n-1;i>=index;i--){
+            if(nums[i]>nums[index])
+            {
+                swap(nums[index],nums[i]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+index+1,nums.end());
+        
        return;
-       }
-       // dusra kaam us idx wale element se just bada element find krna hai.
-       for(int i=n-1;i>idx;i--){
-        if(nums[i]>nums[idx]){
-            swap(nums[i],nums[idx]);
-            break;
-        }
-       }
-       // teesra kaam ab sort krna hai idx element k udhar wala part
-       reverse(nums.begin()+idx+1,nums.end());
-        return;
+       
+
+       
+      
+
+        
     }
 };
