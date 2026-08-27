6d03afadb2928ecbf2fef7b274f94b86cd64f587
Abhishek01yadav <ay0180310@gmail.com>
2026-08-27 17:59:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.52%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
 int dp[46];
-int f(int  n){
-    if(n <=2) return n;
-    if(dp[n]!=-1) return dp[n];
+int f(int  idx){
+    if(idx ==0 || idx==1) return 1;
+    if(dp[idx]!=-1) return dp[idx];
+    int left=f(idx-1);
+    int right=f(idx-2);
+    return dp[idx]=left+right;
 
-    return  dp[n]=f(n-1)+f(n-2);
+  
 
     
 }
