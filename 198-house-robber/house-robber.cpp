c5b2a1ea758b232149f80fb6752733f91945225b
Abhishek01yadav <ay0180310@gmail.com>
2026-08-29 17:13:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (90.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-vector<int>dp;
-int solve(int idx,vector<int>& nums){
-    int n=nums.size();
-    if(idx<0) return 0;
-    if(idx==0 ) return nums[0];
-    if(idx==1) return max(nums[0],nums[1]);
+int dp[101];
+int f(int idx,vector<int>& nums){
+    if(idx==0) return nums[idx];
+    if(idx < 0) return  0;
     if(dp[idx]!=-1) return dp[idx];
-    int a=nums[idx]+solve(idx-2,nums);
-   int b= nums[idx-1]+solve(idx-3,nums);
-    return  dp[idx]=max(a,b);
+
+    int take=f(idx-2,nums)+nums[idx];
+    int skip=f(idx-1,nums);
+    int ans = max(skip,take);
+    return  dp[idx]=ans;
+
 }
-
-int solve2(int idx,vector<int>& nums){
-    if(idx<0) return 0;
-    if(idx==0 ) return nums[0];
-    if(idx==1) return max(nums[0],nums[1]);
-    if(dp[idx]!=-1) return dp[idx];
-    int take=nums[idx]+solve2(idx-2,nums);
-   int skip= solve2(idx-1,nums);
-    return  dp[idx]=max(skip,take);
-}
-
-
     int rob(vector<int>& nums) {
         int n=nums.size();
-        dp=vector<int>(101,-1);
-   
-       return  solve(n-1,nums);
+        memset(dp,-1,sizeof(dp));
+        return f(n-1,nums);
 
         
     }
