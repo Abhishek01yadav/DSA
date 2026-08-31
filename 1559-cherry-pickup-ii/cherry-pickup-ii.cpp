f0e9c9000c5048c4f3d5449cc9e2bdc0be061254
Abhishek01yadav <ay0180310@gmail.com>
2026-09-01 00:54:29 +0530

    Time: 36 ms (84.54%) | Memory: 14.2 MB (84.60%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+int dp[75][75][75];
+int f(int i,int j1,int j2,vector<vector<int>>& grid){
+    int r=grid.size();
+    int c=grid[0].size();
+
+    if( j1<0 || j2<0 || j1>=c || j2>=c) return -1e8;
+
+    if(i==r-1){
+        if(j1==j2) return grid[i][j1];
+        else return grid[i][j1]+grid[i][j2];
+    }
+    if(dp[i][j1][j2]  !=-1) return dp[i][j1][j2];
+
+    int maxi=-1e8;
+
+    for(int dj1=-1;dj1<=+1;dj1++){
+
+        for(int dj2=-1;dj2<=+1;dj2++){
+
+            int value=0;
+            if(j1==j2)  value=grid[i][j1]+f(i+1,j1+dj1,j2+dj2,grid);
+            else {
+                value=grid[i][j1]+ grid[i][j2]+f(i+1,j1+dj1,j2+dj2,grid);
+            }
+            maxi=max(maxi,value);
+
+
+        }
+    }
+
+return  dp[i][j1][j2]=maxi;
+
+
+}
+    int cherryPickup(vector<vector<int>>& grid) {
+         int r=grid.size();
+         memset(dp,-1,sizeof(dp));
+    int c=grid[0].size();
+
+
+        return f(0,0,c-1,grid);
+        
+    }
+};
