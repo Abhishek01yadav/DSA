326f9b62636698414475f9a6204963bb7144930c
Abhishek01yadav <ay0180310@gmail.com>
2026-08-31 22:18:58 +0530

    Time: 6 ms (3.02%) | Memory: 21.2 MB (5.08%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dp[205][10005];
+    int f(int i, int j, vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        if (i == n - 1)
+            return triangle[n - 1][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int d = triangle[i][j] + f(i + 1, j, triangle);
+        int dg = triangle[i][j] + f(i + 1, j + 1, triangle);
+
+        return dp[i][j] = min(d, dg);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        // memset(dp, -1, sizeof(dp));
+        // return f(0, 0, triangle);
+        int n= triangle.size();
+
+        vector < vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int j = 0; j < n; j++) {
+            dp[n - 1][j] = triangle[n - 1][j];
+        }
+
+        for (int i = n-2; i >=0; i--) {
+            for (int j = i; j>=0; j--) {
+
+                int d = triangle[i][j]+dp[i+1][j];
+
+                int dg = triangle[i][j]+dp[i+1][j+1];
+
+
+                 dp[i][j] = min(d, dg);
+            }
+        }
+        return dp[0][0];
+    }
+};
