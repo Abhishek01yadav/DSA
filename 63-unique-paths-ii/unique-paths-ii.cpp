a79738156625ee15e8ba7b47236c919e93c0d9e7
Abhishek01yadav <ay0180310@gmail.com>
2026-08-31 18:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (72.20%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-int dp[105][105];
-    int f(int i, int j, vector<vector<int>>& obstacleGrid) {
-
-        if (i >= 0 && j >= 0 && obstacleGrid[i][j] == 1)
-            return 0;
-        if (i == 0 && j == 0)
-            return 1;
-        if (i < 0 || j < 0)
-            return 0;
-
-            if(dp[i][j]!=-1) return dp[i][j];
-
-
-
-        int up = f(i - 1, j, obstacleGrid);
-
-        int left = f(i, j - 1, obstacleGrid);
-
-        return  dp[i][j]=up + left;
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
-        memset(dp,-1,sizeof(dp));
-        return f(n - 1, m - 1, obstacleGrid);
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+
+
+
+        vector<vector<int>>dp(m,vector<int>(n,0));
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                
+               else  if(i==0 && j==0 ) dp[i][j]=1;
+
+            
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+
+return dp[m-1][n-1];
+        
     }
 };
