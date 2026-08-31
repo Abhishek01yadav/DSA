9febfc71ce501028d2fbd8b28225df7ed01fe382
Abhishek01yadav <ay0180310@gmail.com>
2026-08-31 17:13:35 +0530

    Time: 3 ms (30.11%) | Memory: 15.5 MB (85.19%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-vector<vector<int>>grid2;
 int dp[205][205];
-int m,n;
-int f(  int i,int j){
-    if(i==m-1 && j==n-1) return  grid2[i][j];
-    if(i>=m || j>=n) return 1e9;
-    int ans=0;
-    if (dp[i][j]!=-1) return dp[i][j];
-     int right=grid2[i][j]+f(i+1,j);
-    int down=grid2[i][j]+f(i,j+1);
-    ans+=min(right,down);
-    
-    
-    return dp[i][j]= ans;
-}
+
+    int f(int i,int j,vector<vector<int>>& grid)
+    {
+        if(i==0 && j==0) return grid[0][0]; 
+        if(i<0 || j<0) return 100004 ;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int up= grid[i][j]+f(i-1,j,grid);
+        int left= grid[i][j]+f(i,j-1,grid);
+
+        return  dp[i][j]= min(up,left);
+
+
+    }
+
+
+
 
     int minPathSum(vector<vector<int>>& grid) {
-        grid2=grid;
-         m=grid2.size();
-         n=grid2[0].size();
-         memset(dp,-1,sizeof dp);
-       return   f(0,0);
+        int n=grid.size();
+        int m=grid[0].size();
+        memset(dp,-1,sizeof(dp));
+        return   f(n-1,m-1,grid);
 
         
     }
