9174f5e8aa6fd82e43026d2a31929a23000cdf74
Abhishek01yadav <ay0180310@gmail.com>
2026-09-09 14:40:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (81.44%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-map<pair<int,int>,int>dp;
-int f(int ind,int target,vector<int>& nums){
-    int cntzero=count(nums.begin(),nums.end(),0);
-  
-    if(ind==0){
-        if(nums[0]==0 && target==0 ) return 2;
-        if(target==nums[0] || target==-nums[0]) return 1;
-        return 0;
-    }
 
-   pair<int,int>state={ind,target};
-   if(dp.count(state)) return dp[state];
-    int plus=f(ind-1,target-nums[ind],nums);
-    int minus=f(ind-1,target+nums[ind],nums);
-
-    
-    return  dp[state]=plus+minus;
-}
+int dp[25][1002];
+  int f(int ind,int target,vector<int>& nums){
+      if(ind==0){
+          if(target==0  && nums[0]==0) return 2;
+          if(target==0 || target==nums[0]) return 1;
+          
+          return 0;
+      }
+      if(dp[ind][target] !=-1) return dp[ind][target];
+      
+      int notpick=f(ind-1,target,nums);
+      int pick=0;
+      if(nums[ind]<=target) pick=f(ind-1,target-nums[ind],nums);
+      
+      return dp[ind][target]= pick + notpick;
+  }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-       dp.clear();
-        return f(n-1,target,nums);
-        
+
+         int n=nums.size();
+        memset(dp,-1,sizeof(dp));
+        int tsum=accumulate(nums.begin(),nums.end(),0);
+        if(tsum-target <0) return 0;
+        if((tsum-target)%2 !=0) return 0;
+        int sum=(tsum-target)/2;
+        return f(n-1,sum,nums);
         
     }
 };
