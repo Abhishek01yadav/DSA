adc1cb754ddc3c94cbca88f1d9546e159247418d
Abhishek01yadav <ay0180310@gmail.com>
2026-09-11 00:08:30 +0530

    Time: 3 ms (98.74%) | Memory: 10.5 MB (77.13%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-   
-   
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<long  >> dp(n, vector<long  >(amount+1, 0));
-        for (int amt = 0; amt <= amount; amt++) {
-            if(amt % coins[0] ==0) dp[0][amt] = 1;
-            else dp[0][amt]=0;
-        }
+        vector<unsigned long long> dp(amount + 1, 0);
+        dp[0] = 1;
 
-        for (int ind = 1; ind < n; ind++) {
-            for (int amt = 0; amt <= amount; amt++) {
-
-        long   notpick = dp[ind - 1][amt];
-                long  pick = 0;
-                if (amt >= coins[ind]) {
-                    pick = dp[ind][amt-coins[ind]];
-                }
-
-               dp[ind][amt] = min((long)INT_MAX, pick + notpick);
+        for (int coin : coins) {
+            for (int amt = coin; amt <= amount; amt++) {
+                dp[amt] += dp[amt - coin];
             }
         }
-        return dp[n-1][amount];
+
+        return dp[amount];
+        
     }
 };
