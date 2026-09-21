78393483dbda7756787a951d8b286d259b6edb49
Abhishek01yadav <ay0180310@gmail.com>
2026-09-22 01:42:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (99.61%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,66 +1,32 @@
 class Solution {
 public:
-
-void f(int idx,vector<int>&candidates, vector<int> &temp,int target,vector<vector<int>> &result){
-if(target==0) {
-    result.push_back(temp);
-    return;
-    
-}
-    if(idx==0){
-        if(target % candidates[0] ==0){
-            int count=target/candidates[0];
-            for(int i=1;i<=count;i++){
-            temp.push_back(candidates[0]);
+    void f(int idx, vector<int>& candidates, vector<int>& temp, int target,
+           vector<vector<int>>& result) {
+      
+        if (idx  < 0) {
+            if (target  == 0) {
+                result.push_back(temp);  
             }
-           result.push_back(temp); 
-           
+            return;
+        }
 
-          for(int i=0;i<count;i++){
+        // pick
+
+        if (target >= candidates[idx]) {
+
+            temp.push_back(candidates[idx]);
+            f(idx, candidates, temp, target - candidates[idx], result);
+
             temp.pop_back();
-          }
-           
-           return;
-           
         }
-        return;
-       
-        
-    
 
+        f(idx - 1, candidates, temp, target, result);
     }
-
-    //pick 
-   
-    
-        int currval=candidates[idx];
-
-        if(target-currval >= 0){
-
-     temp.push_back(currval);
-    f(idx,candidates,temp,target-currval,result);
-
-        
-    
-    temp.pop_back();
-        }
-    
-    
-
-    f(idx-1,candidates,temp,target,result);
-    
-    
-
-    
-
-}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>result;
-        vector<int>temp;
-        int n=candidates.size();
-        f(n-1,candidates,temp,target,result);
+        vector<vector<int>> result;
+        vector<int> temp;
+        int n = candidates.size();
+        f(n - 1, candidates, temp, target, result);
         return result;
-
-        
     }
 };
