914f3eea8802cdfc276f2e5e49e575a72b7e922f
Abhishek01yadav <ay0180310@gmail.com>
2026-09-25 16:39:10 +0530

    Time: 2 ms (35.57%) | Memory: 10.8 MB (39.97%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
 vector<vector<int>>ans;
-void f(int idx,vector<int>& nums,vector<int>&ds,vector<int>&freq)
+void f(int idx,vector<int>& nums)
 {
 
 int n=nums.size();
-
-   if(ds.size()==n){
-    ans.push_back(ds);
-   }
-
-for(int i=0;i<n;i++){
-    if(!freq[i]){
-        ds.push_back(nums[i]);
-        freq[i]=1;
-        f(idx,nums,ds,freq);
-        ds.pop_back();
-        freq[i]=0;
-
-    }
+if(idx==n){
+    ans.push_back(nums);
 }
+
+for(int i=idx;i<n;i++){
+    swap(nums[i],nums[idx]);
+    f(idx+1,nums);
+    swap(nums[i],nums[idx]);
+}
+  
 
 
     
 }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int>freq(nums.size(),0);
-        vector<int>ds;
-        f(0,nums,ds,freq);
+       
+        f(0,nums);
         return ans;
         
 
